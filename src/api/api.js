6ee6a3cfae1c7f2412dd6f410a6d4e502d56b016
devansh joshi <devansh0814dj@gmail.com>
2026-02-28 21:53:23 +0530

    backend_api added

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000", // backend base URL
+    baseURL: "https://old-book-selling-backend.vercel.app/", // backend base URL
 });
 
 api.interceptors.request.use(
